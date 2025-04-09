bd6827bcccb582e8d2fed2a00d44093058019cb0
RomarioNaiana <romarionaiana@gmail.com>
2025-04-09 17:02:40 +0000

    finalizado

---
+++ questao1.js
@@ -0,0 +1,13 @@
+const endereco ={
+    logradoura:"rua a",
+    cep:"46500000",
+    estado:"Bahia",
+    municipio:"macaubas",
+    contato:{
+        nome:"naiana",
+        telefone:"77996523985",
+        whatsapp:"+55 996523985",
+    }
+}
+console.log(endereco)
+console.log(endereco.cep)
